e9effaaefd0d0d9002a1070e5ea41702035b5296
dianaveselkina <vess_di@mail.ru>
2024-01-26 21:34:26 +0300

    style slider

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let offset = 0;
 const sliderLine = document.querySelector('.project__slider');
 document.querySelector('.slider__next').addEventListener('click', function () {
   offset = offset + 602;
-  if (offset > 4214) {
+  if (offset > 3612) {
     offset = 0;
   }
-  sliderLine.style.left = offset + 'px';
+  sliderLine.style.left = -offset + 'px';
+});
+document.querySelector('.slider__prev').addEventListener('click', function () {
+  offset = offset - 602;
+  if (offset < 0) {
+    offset = 3612;
+  }
+  sliderLine.style.left = -offset + 'px';
 });
