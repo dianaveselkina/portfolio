93f6b3f10fdfb3a78abd4f754e1df1953bbfda78
dianaveselkina <vess_di@mail.ru>
2024-02-09 22:53:38 +0300

    change slider mobil

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let offset = 0;
 const sliderLine = document.querySelector('.project__slider');
 document.querySelector('.slider__next').addEventListener('click', function () {
-  offset = offset + 602;
+  offset = offset + 480;
   if (offset > 3612) {
     offset = 0;
   }
   sliderLine.style.left = -offset + 'px';
 });
 document.querySelector('.slider__prev').addEventListener('click', function () {
-  offset = offset - 602;
+  offset = offset - 480;
   if (offset < 0) {
     offset = 3612;
   }
