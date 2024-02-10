fafeda076125360629e7bc04bc8b1d6cc740133a
dianaveselkina <vess_di@mail.ru>
2024-02-10 17:52:35 +0300

    style mobil

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let offset = 0;
 const sliderLine = document.querySelector('.project__slider');
 document.querySelector('.slider__next').addEventListener('click', function () {
-  offset = offset + 480;
+  offset = offset + 365;
   if (offset > 3612) {
     offset = 0;
   }
   sliderLine.style.left = -offset + 'px';
 });
 document.querySelector('.slider__prev').addEventListener('click', function () {
-  offset = offset - 480;
+  offset = offset - 365;
   if (offset < 0) {
     offset = 3612;
   }
