5d478fe4657d44619d11170b9a3aeabb9a031f46
dianaveselkina <vess_di@mail.ru>
2024-02-23 19:35:05 +0300

    add game-alien-raid

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let offset = 0;
 const sliderLine = document.querySelector('.project__slider');
 document.querySelector('.slider__next').addEventListener('click', function () {
-  offset = offset + 365;
+  offset = offset + 602;
   if (offset > 3612) {
     offset = 0;
   }
   sliderLine.style.left = -offset + 'px';
 });
 document.querySelector('.slider__prev').addEventListener('click', function () {
-  offset = offset - 365;
+  offset = offset - 602;
   if (offset < 0) {
     offset = 3612;
   }
