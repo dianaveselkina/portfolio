c54024fd9e74bcbfaf846b832ae88f6ef15693f6
dianaveselkina <vess_di@mail.ru>
2024-03-21 23:23:41 +0300

    fix slider

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-let offset = 0;
-const sliderLine = document.querySelector('.project__slider');
-document.querySelector('.slider__next').addEventListener('click', function () {
-  offset = offset + 602;
-  if (offset > 3612) {
-    offset = 0;
-  }
-  sliderLine.style.left = -offset + 'px';
-});
+const images = document.querySelectorAll('.project');
+const sliderLine = document.querySelector('.slider__line');
+let count = 0;
+let width;
+
+function init() {
+  console.log('resize');
+  width = document.querySelector('.slider').offsetWidth;
+
+  sliderLine.style.width = width * images.length + 'px';
+  images.forEach((item) => {
+    item.style.width = width + 'px';
+  });
+  rollSlider();
+}
+window.addEventListener('resize', init);
+init();
+
 document.querySelector('.slider__prev').addEventListener('click', function () {
-  offset = offset - 602;
-  if (offset < 0) {
-    offset = 3612;
+  count--;
+  if (count < 0) {
+    count = images.length - 1;
   }
-  sliderLine.style.left = -offset + 'px';
+  rollSlider();
 });
+
+document.querySelector('.slider__next').addEventListener('click', function () {
+  count++;
+  if (count >= images.length) {
+    count = 0;
+  }
+  rollSlider();
+});
+function rollSlider() {
+  sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
